7c4b4e5141c8737479650fad6ff0f414f33a0823
Phil Crawford <zombeerose@yahoo.com>
2012-11-05 14:53:20 -0700

    Eliminate required portal classes from model template

--- tpl/model.t.js
+++ tpl/model.t.js
@@ -2,12 +2,10 @@ StartTest(function(t){
     var Ext = t.getExt(),
         cls = '${class}';
 
-    t.requireOk(['Ext.ux.portal.util.Format','Ext.ux.portal.util.ResourceTypes'],function(){
-        t.createModelAndThen(cls,function(model){
-            t.loadModelAndThen(model,function(model){
-                t.validateModelAndThen(model,function(model){
-                    t.done();
-                });
+    t.createModelAndThen(cls,function(model){
+        t.loadModelAndThen(model,function(model){
+            t.validateModelAndThen(model,function(model){
+                t.done();
             });
         });
     });
