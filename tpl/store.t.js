002839936dfb16a59c87495fd623ce1ef10cbc22
Philip Crawford <philip.crawford@dvusd.org>
2022-12-05 16:09:32 -0700

    DRY the store template

--- tpl/store.t.js
+++ tpl/store.t.js
@@ -1,12 +1,8 @@
 StartTest(function(t){
     var cls = '${class}';
 
-    t.createStoresAndThen(cls,function(){
-        var store = Ext.getStore(cls);
-        t.is(!!store,true,'Retrieved store from StoreManager');
-        
-        t.loadStoresAndThen(store,function(){
-            t.done();
-        });
-    });
+    t.chainDone(
+        'createStoresAndThen("'+cls+'")',
+        'loadStoreRowsAndThen("'+cls+'")',
+    );
 });
